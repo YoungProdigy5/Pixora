74fce24cac000b9cdca4cf3ab39162569c5b9456
YoungProdigy5 <fancylilp@gmail.com>
2026-09-17 00:02:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// =========================================================
+// PIXORA — SUPABASE CONNECTION
+// =========================================================
+
+const SUPABASE_URL = "hlldyvzgvblxmnygldqd";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_mg6WiPQq2BTk_2YTdJkaJA_f4zQwPF-";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
 /* =========================================================
    PIXORA — SCALABLE WALLPAPER ENGINE
    Supports 50 wallpapers per category now
